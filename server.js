46f763061f19cfab9733808564054aeb9a8a109c
Davitmach <wvime30@gmail.com>
2025-04-24 18:50:58 +0400

    refactor: migrate from node-telegram-bot-api to telegraf for improved bot functionality
    - Updated package.json to include telegraf and removed node-telegram-bot-api.
    - Refactored server.js to implement Telegraf for handling Telegram bot interactions.
    - Changed the way the bot responds to the /start command using inline keyboard buttons.
    - Configured webhook settings for the bot to launch on a specified domain and port.

--- server.js
+++ server.js
@@ -1,25 +1,23 @@
-import TelegramBot from 'node-telegram-bot-api';
 
-const bot = new TelegramBot("7311122619:AAHJk7WdmfWEMYqGAMPry2ybIAMIgNrC3W0", { polling: true });
 
-bot.onText(/\/start/, (msg) => {
-  const chatId = msg.chat.id;
+import { Telegraf, Markup } from 'telegraf';
 
-  // Кнопка с Web App (мини-приложением)
-  const options = {
-    reply_markup: {
-      inline_keyboard: [
-        [
-          {
-            text: 'Открыть мини-приложение',
-            web_app: {
-              url: 'https://task-battle.vercel.app/'  // URL вашего мини-приложения
-            }
-          }
-        ]
-      ]
-    }
-  };
+const bot = new Telegraf('7311122619:AAHJk7WdmfWEMYqGAMPry2ybIAMIgNrC3W0');
 
-  bot.sendMessage(chatId, 'Привет! Нажми на кнопку, чтобы открыть мини-приложение.', options);
+bot.start((ctx) => {
+  ctx.reply(
+    'Привет! Нажми на кнопку, чтобы открыть мини-приложение.',
+    Markup.inlineKeyboard([
+      Markup.button.webApp('Открыть мини-приложение', 'https://task-battle.vercel.app/'),
+    ])
+  );
+});
+const DOMAIN = 'https://taskbattleserver.onrender.com'; 
+const TOKEN = '7311122619:AAHJk7WdmfWEMYqGAMPry2ybIAMIgNrC3W0'; 
+bot.launch({
+  webhook: {
+    domain: DOMAIN,
+    port:  3002, 
+    hookPath: `/${TOKEN}`
+  }
 });
